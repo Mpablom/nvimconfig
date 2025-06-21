d9803e906cb216cfde3fbf20f93482b06b08da55
pablo <pablomoya211@gmail.com>
2025-06-21 01:32:36 -0300

    agregado coopilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "github/copilot.vim",
+    lazy = false, -- Carga inmediatamente al iniciar Neovim
+    config = function()
+      -- Configuración básica (opcional)
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+
+      -- Atajos de teclado personalizados
+      vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {
+        silent = true,
+        expr = true,
+        script = true,
+        replace_keycodes = false,
+      })
+      vim.keymap.set("i", "<C-K>", "<Plug>(copilot-next)")
+      vim.keymap.set("i", "<C-L>", "<Plug>(copilot-previous)")
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = "nvim-cmp",
+    config = function()
+      require("copilot_cmp").setup({
+        formatters = {
+          insert_text = function(text)
+            return text:gsub("\n", " ")
+          end,
+        },
+      })
+    end,
+  },
+}
