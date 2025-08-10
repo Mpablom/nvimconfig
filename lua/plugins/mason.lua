ea2d98faa7a0649e5ad428bf0fd75a710c206361
Pablo <pablomoya211@gmail.com>
2025-08-10 01:40:22 -0300

    several plugins update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,19 @@
 return {
   { "williamboman/mason.nvim", version = "1.11.0" },
   { "williamboman/mason-lspconfig.nvim", version = "1.32.0" },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "lua-language-server",
+          "pyright",
+          "bash-language-server",
+          "stylua",
+        },
+        auto_update = true,
+        run_on_start = true,
+      })
+    end,
+  },
 }
